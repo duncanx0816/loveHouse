be343a78a8456f62b42ec2c187690e8737e05b9d
duncanx0816 <xiongdengkun@163.com>
2019-01-07 21:45:48 +0800

    update

--- note/models.py
+++ note/models.py
@@ -3,12 +3,19 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from datetime import datetime
 
+
 class Note(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     body = models.TextField()
     created = models.DateField(auto_now_add=True)
 
+    class Meta:
+        ordering = ("-created",)
+
     @property
     def created_str(self):
         return datetime.strftime(self.created, '%y/%m/%d')
 
+    @property
+    def gender(self):
+        return self.author.username == 'Duncan'
--- note/views.py
+++ note/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, TemplateView, ListView, DeleteView
-from braces.views import LoginRequiredMixin
+from braces.views import LoginRequiredMixin, CsrfExemptMixin
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+
+
+from random import randint, shuffle
+from logzero import logger
 
 from .models import Note
 from .forms import NewNoteForm
 
 
-class NoteMixin(LoginRequiredMixin):
+class NoteMixin(CsrfExemptMixin, LoginRequiredMixin):
     model = Note
     login_url = '/login/'
 
@@ -31,7 +36,27 @@ class ListNote(NoteMixin, ListView):
 
     def get_queryset(self):
         qs = super(ListNote, self).get_queryset()
-        return qs.filter(author=self.request.user)
+        return qs  # .filter(author=self.request.user)
+
+    def get_context_data(self, *args, **kargs):
+        context = super().get_context_data(*args, **kargs)
+        context['left'] = randint(0, 30)
+        context['top'] = randint(0, 30)
+        return context
+
+    def post(self, request):
+        res = []
+        for note in self.get_queryset().order_by('-created')[:9]:
+            res.append({
+                'gender': note.gender,
+                'left': randint(-30, 30),
+                'top': randint(-15, 15),
+                'body': note.body
+            })
+        logger.info(len(res))
+        logger.info(res)
+        shuffle(res)
+        return JsonResponse(res, safe=False)
 
 
 class HistNote(NoteMixin, ListView):
